cb6bb2dbbde63d75876bf26faa9a5fe1cabf5d2f
NatzNatz <125758574+QQUway@users.noreply.github.com>
2024-05-13 20:03:25 +0700

    rusak njir

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Siswapa;
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+
 
 class ProfileController extends Controller
 {
@@ -16,8 +18,13 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        // Fetch data from the database
+        $students = Siswapa::all();
+
+        // Pass the fetched data to the view
         return view('profile.edit', [
             'user' => $request->user(),
+            'students' => $students,
         ]);
     }
 
