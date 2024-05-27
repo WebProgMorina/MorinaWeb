785b740e7488346660f63ec415e0917a29c434a3
NatzNatz <125758574+QQUway@users.noreply.github.com>
2024-05-27 22:59:33 +0700

    fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -44,7 +44,7 @@
                             </x-nav-link>
                         </div>
                         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                            <x-nav-link :href="route('library')" :active="request()->routeIs('library')">
+                            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                                 {{ __('Library') }}
                             </x-nav-link>
                         </div>
